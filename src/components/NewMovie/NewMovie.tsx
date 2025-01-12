64dd5dc685ee070cf3d359834990aacb038305de
Oleksii Karpov <alneon1@gmail.com>
2025-01-12 13:56:11 +0200

    added solution v.2

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -14,11 +14,9 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
+  const [isFormValid, setIsFormValid] = useState(true);
   const [isImgUrlValid, setIsImgUrlValid] = useState(true);
   const [isImdbUrlValid, setIsImdbUrlValid] = useState(true);
-
-  const isFormValid =
-    title !== '' && imgUrl !== '' && imdbUrl !== '' && imdbId !== '';
 
   const pattern = new RegExp(
     '^(' +
@@ -32,9 +30,12 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
+    const FormValid =
+      title !== '' && imgUrl !== '' && imdbUrl !== '' && imdbId !== '';
     const imgUrlValid = pattern.test(imgUrl);
     const imdbUrlValid = pattern.test(imdbUrl);
 
+    setIsFormValid(FormValid);
     setIsImgUrlValid(imgUrlValid);
     setIsImdbUrlValid(imdbUrlValid);
 
